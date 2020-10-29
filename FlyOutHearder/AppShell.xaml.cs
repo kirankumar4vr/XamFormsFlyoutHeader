4a6c7e5f5b76a57806fd954db380deb6e87d70ad
Kiran Anantapalli <Kiran.Anantapalli@crump.com>
2020-10-29 15:29:13 -0400

    firstcommit

---
+++ FlyOutHearder/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using FlyOutHearder.ViewModels;
+using FlyOutHearder.Views;
+using Xamarin.Forms;
+
+namespace FlyOutHearder
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ FlyOutHearder/Views/FlyoutMenuHeader.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FlyOutHearder.Views
+{
+    public partial class FlyoutMenuHeader : ContentView
+    {
+        public FlyoutMenuHeader()
+        {
+            InitializeComponent();
+        }
+        void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+        {
+            Shell.Current.FlyoutIsPresented = false;
+        }
+    }
+}
